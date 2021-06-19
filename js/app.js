fff14d7a575d565606e5f3aab5e000cb687271a9
KaiYanagisawa <12730ky@gmail.com>
2021-06-19 18:52:32 +0900

    条件にあった音声出力

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+var sorry = document.getElementById("sorry");
+var ice = document.getElementById("ice");
+var medicine = document.getElementById("medicine");
+var likeyou = document.getElementById("likeyou");
+var potato = document.getElementById("potato");
+var yammy = document.getElementById("yammy");
+
+
 var btn = document.getElementById('voice-btn');
 var content = document.getElementById('content');
 
@@ -15,4 +23,11 @@ speech.addEventListener('result', function(e) {
     console.log(e);
     var text = e.results[0][0].transcript;
     content.textContent = text;
+    if (text == "お昼何食べたい") {
+        potato.play();
+    } else if (text == "付き合ってください") {
+        sorry.play();
+    } else if (text == "美味しい") {
+        yammy.play();
+    }
 });
