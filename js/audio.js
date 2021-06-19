e2aa948a6791742431c6cef90817734be1f56b68
KaiYanagisawa <12730ky@gmail.com>
2021-06-19 13:57:30 +0900

    fix audio

--- js/audio.js
+++ js/audio.js
@@ -1,11 +1,5 @@
-const play_btn = document.querySelector('#talker-alternative');
-
-// //audio file
-let sound = new Audio("https://raw.githubusercontent.com/Yousuke777/sound/main/kansei.mp3");
-
-// //play event
-play_btn.addEventListener('click', play);
-
-function play() {
-    sound.play();
-}
+var audio = document.getElementById("sorry");
+var button = document.getElementById("talker-alternative");
+button.addEventListener("click", function() {
+    audio.play();
+}, false);
